bb0de6ab4bfab2ba8fcb639e89fefa3eaf736624
Михаил Беляков <misa.belyakov.ru@gmail.com>
2025-03-11 23:02:41 +0700

    Deleted rubbish

--- HW/Task1/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Task1_FILE C:/Users/User/Desktop/Module36/HW/Task1/cmake-build-debug/Task1.exe)
-set(__QT_DEPLOY_TARGET_Task1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Task1_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
--- HW/Task1_vol_2/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Task1_vol_2_FILE C:/Users/User/Desktop/Module36/HW/Task1_vol_2/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/Task1_vol_2.exe)
-set(__QT_DEPLOY_TARGET_Task1_vol_2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Task1_vol_2_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
